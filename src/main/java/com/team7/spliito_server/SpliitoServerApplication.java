ce78f67914e2a44d4031a6b8c6ab57ae10a0aa90
han16935 <han1693516@gmail.com>
2024-10-25 16:56:20 +0900

    first commit

---
+++ src/main/java/com/team7/spliito_server/SpliitoServerApplication.java
@@ -0,0 +1,13 @@
+package com.team7.spliito_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpliitoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpliitoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team7/spliito_server/SpliitoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.spliito_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpliitoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

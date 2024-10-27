1543221a42603295802f0d4616d26e813980aa97
han16935 <han1693516@gmail.com>
2024-10-27 17:07:49 +0900

    refactor/GroupService 상단에 @RequiredArgsConstructor, @Transactional(readOnly = true) 추가 + lambda 사용 + toList() 사용

--- src/main/java/com/team7/spliito_server/service/GroupService.java
+++ src/main/java/com/team7/spliito_server/service/GroupService.java
@@ -6,22 +6,20 @@ import com.team7.spliito_server.model.Group;
 import com.team7.spliito_server.model.User;
 import com.team7.spliito_server.repository.GroupRepository;
 import com.team7.spliito_server.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class GroupService {
     private final GroupRepository groupRepository;
     private final UserRepository userRepository;
-
-    public GroupService(GroupRepository groupRepository, UserRepository userRepository) {
-        this.groupRepository = groupRepository;
-        this.userRepository = userRepository;
-    }
 
     /**
      * 그룹 생성 또는 업데이트
@@ -39,14 +37,14 @@ public class GroupService {
         List<String> existingMemberNames = group.getMembers() != null ?
                 group.getMembers().stream()
                         .map(User::getName)
-                        .collect(Collectors.toList())
+                        .toList()
                 : List.of(); // 새 그룹이면 빈 리스트로 초기화
 
         // 요청된 멤버 이름 중 기존에 없는 멤버만 추가
         List<User> newMembers = request.getMemberNames().stream()
                 .filter(name -> !existingMemberNames.contains(name)) // 기존 멤버와 중복되지 않는 이름만 추가
                 .map(name -> new User(name, group))
-                .collect(Collectors.toList());
+                .toList();
 
         // 그룹에 멤버 추가
         group.getMembers().addAll(newMembers);
@@ -58,7 +56,6 @@ public class GroupService {
         return "group/" + savedGroup.getId();
     }
 
-    @Transactional(readOnly = true)
     public List<GroupResponse> getAllGroups() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
@@ -68,9 +65,9 @@ public class GroupService {
                         group.getName(),
                         group.getCreatedDate().format(formatter),
                         group.getMembers().stream()
-                                .map(member -> member.getName())
-                                .collect(Collectors.toList())
+                                .map(User::getName)
+                                .toList()
                 ))
-                .collect(Collectors.toList());
+                .toList();
     }
 }

a242373b6ad89ae51eae28e7020e5a8ec81883c0
han16935 <han1693516@gmail.com>
2024-10-27 16:59:40 +0900

    refactor/Group entity에서 baseEntity 상속받도록, @Getter, @NoArgsConstructor 추가, createdDate 추가,

--- src/main/java/com/team7/spliito_server/model/Group.java
+++ src/main/java/com/team7/spliito_server/model/Group.java
@@ -1,17 +1,19 @@
 package com.team7.spliito_server.model;
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "`group`") // 예약어 충돌 방지
-@EntityListeners(AuditingEntityListener.class) // Auditing 기능 활성화
-public class Group {
+public class Group extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)  // ID 자동 생성
@@ -19,42 +21,16 @@ public class Group {
 
     private String name; // 그룹 이름
 
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdDate;  // 생성일
+    private LocalDateTime createdDate;
 
     // User 엔티티와의 일대다 관계 매핑
     @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<User> members = new ArrayList<>();
 
-    public Group() {}
+
 
     public Group(String name) {
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public List<User> getMembers() {
-        return members;
     }
 
     public void setMembers(List<User> members) {

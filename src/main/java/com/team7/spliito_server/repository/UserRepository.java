bcc4b696493543bdbeffaced60536534c9033451
han16935 <han1693516@gmail.com>
2024-10-27 19:54:33 +0900

    feature/findByGroupId() 추가

--- src/main/java/com/team7/spliito_server/repository/UserRepository.java
+++ src/main/java/com/team7/spliito_server/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.team7.spliito_server.repository;
 
 import com.team7.spliito_server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByGroupId(Long groupId);
 }

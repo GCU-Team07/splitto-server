a317b7aab636eb0d7c9ee924020485b80407684d
han16935 <han1693516@gmail.com>
2024-10-27 15:24:41 +0900

    test/IntegrationTestSupport에 TestContainer 관련 작성

---
+++ src/test/java/com/team7/spliito_server/IntegrationTestSupport.java
@@ -0,0 +1,27 @@
+package com.team7.spliito_server;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+    private static final String MYSQL_VERSION = "mysql:latest";
+
+    private static final MySQLContainer<?> mysql;
+
+    static {
+        mysql = new MySQLContainer<>(MYSQL_VERSION);
+        mysql.start();
+    }
+
+    @DynamicPropertySource
+    public static void dynamicConfiguration(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
+}

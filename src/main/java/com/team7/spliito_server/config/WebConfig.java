cecf5a1e761d40f7fc10fa2ae993f4da18c7a8ac
han16935 <han1693516@gmail.com>
2024-10-28 12:18:48 +0900

    chore/CORS 및 validation 설정 (@PathVariable validation 위해 입력)

---
+++ src/main/java/com/team7/spliito_server/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.team7.spliito_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "PATCH");
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+}
